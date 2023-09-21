f380afff7a6c04d226192fe3b13436bea63b6a2e
ftijan <41797259+ftijan@users.noreply.github.com>
2023-09-21 21:59:24 +0100

    Merge basic solution example (#1)
    * create solution, docker compose file
    * add sample code and docs

---
+++ src/Example.Neo4jApp/Movie.cs
@@ -0,0 +1,9 @@
+﻿namespace Example.Neo4jApp
+{
+	internal class Movie
+	{
+        public string Title { get; set; }
+        public string Tagline { get; set; }
+        public long Release { get; set; }
+	}
+}
