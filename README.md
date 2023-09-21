939603119a8b120d938c0d11f7205ad16fcd1246
Filip Tijan <ftijan@gmail.com>
2023-09-21 21:58:36 +0100

    add sample code and docs

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# neo4j-dotnet
-An example of a .Net app connecting to Neo4j graph DB
+# neo4j .Net App Example
+A quick sample app showing how to:
+- Run a local neo4j instance from a Docker compose file
+- Connect to the local neo4j instance 
+- Create a session
+- Run a simple query
+- Map the data
+
+Based on a longer neo4j .Net sample app found [here](https://github.com/neo4j-examples/movies-dotnetcore-bolt/)
+
+To recreate the sample `Movies` dataset:
+- Make sure the docker image instance is running
+- Navigate to the local neo4j DB browser app at [http://localhost:7474](http://localhost:7474)
+- From the left-hand menu, navigate to the `Neo4j Browser Guides` page
+- Select the `:guide movie-graph` section link and follow the instructions
--- src/Example.Neo4jApp/Program.cs
+++ src/Example.Neo4jApp/Program.cs
@@ -1,10 +1,45 @@
-﻿namespace Example.Neo4jApp
+﻿using Neo4j.Driver;
+
+namespace Example.Neo4jApp
 {
 	internal class Program
 	{
-		static void Main(string[] args)
+		static async Task Main(string[] args)
 		{
-			
-		}
+			// Assumes a local neo4j DB exposed on port 7687, user "neo4j", no password
+			var driver = GraphDatabase.Driver(
+				"bolt://localhost:7687/",
+				AuthTokens.Basic("neo4j", ""));
+
+			var movies = await GetMoviesByTitle(driver, "Apollo 13");
+
+			foreach (var movie in movies) 
+			{
+				Console.WriteLine($"Found movie: {movie.Title}\nWith Tagline: {movie.Tagline}\nReleased in: {movie.Release}");
+			}			
+        }
+
+		private static async Task<List<Movie>> GetMoviesByTitle(IDriver driver, string movieTitle)
+		{
+			await using var session = driver.AsyncSession(builder => builder.WithDatabase("neo4j"));
+
+			return await session.ExecuteReadAsync(async transaction =>
+			{
+				var cursor = await transaction.RunAsync(
+@"
+	MATCH (movie:Movie)
+    WHERE toLower(movie.title) CONTAINS toLower($title)
+    RETURN movie.title AS title,
+            movie.released AS released,
+            movie.tagline AS tagline", new { title = movieTitle });
+
+				return await cursor.ToListAsync(record => new Movie
+				{ 
+					Title = record["title"].As<string>(),
+					Tagline = record["tagline"].As<string>(),
+					Release = record["released"].As<long>()
+				});
+			});
+		}		
 	}
 }
